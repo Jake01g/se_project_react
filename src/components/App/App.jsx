9bfcc37a609b049f5a83bb53a1b1422131ca78a0
Jake Glans <165740770+Jake01g@users.noreply.github.com>
2025-04-03 16:37:04 -0700

    new garment modal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
@@ -11,7 +13,49 @@ function App() {
       <div className="page__content">
         <Header />
         <Main weatherData={weatherData} />
+        <Footer />
       </div>
+      <ModalWithForm title="New Garment" buttonText="Add garment">
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label-type-radio">
+            <input id="hot" type="radio" className="modal__radio-input" />
+            Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label-type-radio"
+          >
+            <input id="warm" type="radio" className="modal__radio-input" />
+            Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label-type-radio"
+          >
+            <input id="cold" type="radio" className="modal__radio-input" />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer className="footer">Developed by Jake Glans</footer>;
+}
+
+export default Footer;
