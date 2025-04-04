6f0124dbea42859415c35e8adb55768c6a10a999
Jake Glans <165740770+Jake01g@users.noreply.github.com>
2025-04-04 00:38:18 -0700

    modal styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,14 @@
+import "./Footer.css";
+
 function Footer() {
-  return <footer className="footer">Developed by Jake Glans</footer>;
+  return (
+    <footer className="footer">
+      {/* <div className="footer__content"> */}
+      <p className="footer__name">Developed by Jake Glans</p>
+      <p className="footer__year">2025</p>
+      {/* </div> */}
+    </footer>
+  );
 }
 
 export default Footer;
